36dc04fc454bcb6913f37e42cee6a0b8cdaacadb
sublun <sublun@126.com>
2018-05-12 17:35:48 +0800

    添加test工程至git

---
+++ idea-git-test/src/com/ithema/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.ithema.git;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+    }
+}
